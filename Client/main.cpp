d5d7102311fc7edea26a5ac8cdb4cd0ced454fe7
Pawel <Pawel@Pawel-Komputer>
2010-10-14 19:45:32 +0200

    Inicjalizacja Client

---
+++ Client/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Pawel
+ *
+ * Created on 14 październik 2010, 19:44
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
